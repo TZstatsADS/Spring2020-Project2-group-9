0874c48b72a46f5fe04c90a9a2915b258d76f3ab
RanRenaLu <rl3077@columbia.edu>
2020-02-20 23:45:11 -0500

    Map Updates

--- app/server.R
+++ app/server.R
@@ -2,13 +2,21 @@
 library(shiny)
 library(leaflet)
 library(readr)
+library(lubridate)
 shinyServer(function(input,output, session){
-  load("../data/na_drop.RData")
-   category <- reactive(na_drop[which(na_drop$category==input$category),])
+   load("../data/na_drop.RData")
+   na_drop$`Posting Date` <- as.Date(na_drop$`Posting Date`, "%m/%d/%Y")
+   
+   category <- reactive(na_drop[which(na_drop$category %in% input$category & na_drop$`Full/Part` %in% input$`Full/Part`&
+                                      na_drop$`Career Level` %in% input$`Career Level` & na_drop$borough %in% input$borough &
+                                      na_drop$`Posting Date` >= input$`Posting Date` & na_drop$`Salary Range To` >= input$`Salary Range To`),])
+
    output$map <- renderLeaflet({
-     map <- leaflet() %>% addTiles() %>% addCircleMarkers(lng = category()$lon, lat = category()$lat,
+     map <- leaflet() %>% setView(-73.9578,40.72348,zoom = 13) %>% addTiles() %>% addCircleMarkers(lng = category()$lon, lat = category()$lat, 
                                                           clusterOptions = markerClusterOptions()) %>%
        addProviderTiles(providers$CartoDB.Positron)
+     
+     
    })
 
 })
--- app/ui.r
+++ app/ui.r
@@ -8,7 +8,7 @@ r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
 ###
 load("../data/na_drop.RData")
-
+na_drop$`Posting Date` <- as.Date(na_drop$`Posting Date`, "%m/%d/%Y")
 
 dashboardPage(
   dashboardHeader(title = "NYC Government Job Posting"),
@@ -38,42 +38,40 @@ dashboardPage(
             )),
     # MAP
     tabItem(tabName = "MAP",
-      fluidPage(
-        # wellPane(
-          # selectInput("category", label = h3("Select Category"),
-          #             choices = list("Category" = list("Operation & Maintenance", "Finance","Public Safety",
-          #                                              "Clerical & Administrative Support", "Technology & Data",
-          #                                              "Community","Social Service","Health","Policy, Research & Analysis",
-          #                                              "Engineering, Architecture, & Planning","Communications & Intergovernmental",
-          #                                              "Legal"))),
-          #             leafletOutput("map")
-        
-        
-        checkboxGroupInput("category", "Choose Category:",
-                           choiceNames =
-                             list("Operation & Maintenance", "Finance","Public Safety",
-                                  "Clerical & Administrative Support", "Technology & Data",
-                                  "Community","Social Service","Health","Policy, Research & Analysis",
-                                  "Engineering, Architecture, & Planning","Communications & Intergovernmental",
-                                  "Legal"
-                                  ),
-                           choiceValues =
-                             list("Operation & Maintenance", "Finance","Public Safety",
-                                  "Clerical & Administrative Support", "Technology & Data",
-                                  "Community","Social Service","Health","Policy, Research & Analysis",
-                                  "Engineering, Architecture, & Planning","Communications & Intergovernmental",
-                                  "Legal")
-        ),
-        leafletOutput("map")
-        
-        
-        
-                      )
-        )
-      # )
+            fluidPage(
+ 
+              leafletOutput("map",width="100%",height=750),
+              absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
+                            top = 140, left = 250, right = "auto", bottom = "auto", width = 250, height = "auto",
+                checkboxGroupInput("category", "Choose Category:",
+                                 choices = c("Operation & Maintenance", "Finance","Public Safety",
+                                        "Clerical & Administrative Support", "Technology & Data",
+                                        "Community","Social Service","Health","Policy, Research & Analysis",
+                                        "Engineering, Architecture, & Planning","Communications & Intergovernmental",
+                                        "Legal"),
+                                 selected = "Operation & Maintenance"),
+                checkboxGroupButtons("Full/Part", "Choose Part/Full Time:", choices = c("F","P"), selected = c("F","P")),
+                checkboxGroupInput("borough", "Choose Borough:",
+                                   choices = c("Bronx","Queens", "Manhattan", "Brooklyn", "Staten Island"), 
+                                   selected = c("Bronx","Queens", "Manhattan", "Brooklyn", "Staten Island"))
+              ),
+              absolutePanel(id = "controls", class = "panel panel-default", fixed= TRUE, draggable = TRUE,
+                            top = 140, left = "auto", right = 20, bottom = "auto", width = 320, height = "auto",
+                            selectInput("Career Level", label = h3("Select Career Level"), 
+                                        choices = list("Career Level" = list("Entry-Level", "Executive","Experienced (non-manager)",
+                                                                         "Manager", "Student")), selected = "Entry-Level"),
+                            dateInput("Posting Date", "Choose start date:",
+                                     value = "2020-01-01",
+                                     min = min(na_drop$`Posting Date`),
+                                     max = max(na_drop$`Posting Date`)),
+                            sliderInput("Salary Range To", "Highest Salary Offered Higher Than: ", min=floor(min(na_drop$`Salary Range To`)),
+                                        max=floor(max(na_drop$`Salary Range To`)), value=2000, step=20)
+                            
+              )
+            )
+    )
+    ### MAP Part Done
     
-    ####
   )
-  
-  ))
-
+  )
+  )
